b2c75a8abcb4521786f6a16b4dea4a2193570f09
yukio.matsuda <ever.after.0407@icloud.com>
2021-02-17 00:49:11 +0900

    Add Report index

--- src/models/Report.java
+++ src/models/Report.java
@@ -23,7 +23,7 @@ import javax.persistence.Table;
             query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
     ),
     @NamedQuery(
-            name = "getReportscount",
+            name = "getReportsCount",
             query = "SELECT COUNT(r) FROM Report AS r"
     ),
 
